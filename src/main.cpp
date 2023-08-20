accfc4cba83c31a28c5cf140cd9de99d892b0f06
MRPRO77 <mrpro77@hotmail.com>
2023-08-20 20:22:14 -0300

    Login Mega_ADK_TCA9548A

---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+/*******************************************************************************
+   Título:      TCC  Engenharia Mecatrônica.
+
+   Descrição:   C L P   ---  I H M
+
+   Processador: ARDUINO MEGA ADK + ARDUINO UNO + ROBOCORE ESP32 
+   
+
+
+   Desenvolvido: MRPRO Tecnologia e Automação
+
+   Autor: Tecnologo. Marcelo Rodrigues     Crea-SP 5070505617
+
+   Data de Inicio: 20/08/2023       Versão:   2023.1.0
+
+        
+
+ 
+*******************************************************************************/  
+/* ============================================================================  
+                                                              
+                                       _                      
+                                      / \                     
+                                     |oo >                    
+                                     _\=/_                    
+                    ___         #   /  _  \   #               
+                   /<> \         \\//|/.\|\\//                
+                 _|_____|_        \/  \_/  \/                 
+                | | === | |          |\ /|                    
+                |_|  0  |_|          \_ _/                    
+                 ||  0  ||           | | |                    
+                 ||__*__||           | | |                    
+                |* \___/ *|          []|[]                    
+                /=\ /=\ /=\          | | |                    
+________________[_]_[_]_[_]_________/_]_[_\_____________________________________
+                                                                             
+===================================================================================================== */
+
+// ========================================================================================================
+// --- Bibliotecas Auxiliares ---
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "Wire.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_BusIO_Register.h>
+#include <TCA9548A.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_SSD1306.h>
+
+
+
+void setup() {
+
+
+ 
+}
+
+void loop() {
+
+
+
+}
+
